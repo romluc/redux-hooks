b6574a3563ea3ef5bab1d76cbdc984e988742751
romluc <romulofilhofilho@gmail.com>
2019-10-17 11:09:04 -0300

    Add hooks to Component

--- src/components/CourseList/index.js
+++ src/components/CourseList/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function CourseList() {
 	const courses = useSelector(state => state.data);
+	const dispatch = useDispatch();
 
+	function addCourseAction() {
+		return { type: 'ADD_COURSE', title: 'GraphQL' };
+	}
+	function addCourse() {
+		dispatch(addCourseAction('GraphQL'));
+	}
 	return (
-		<ul>
-			{courses.map(course => (
-				<li key={course}>{course}</li>
-			))}
-		</ul>
+		<>
+			<ul>
+				{courses.map(course => (
+					<li key={course}>{course}</li>
+				))}
+			</ul>
+			<button type='button' onClick={addCourse}>
+				Add course
+			</button>
+		</>
 	);
 }
